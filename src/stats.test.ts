82bbfa68dd676118529d7ae1b65070185e9e9d4e
Darío Beiró <dario.beiro@dxc.com>
2023-04-04 17:35:49 +0200

    Async testing

--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,5 +1,6 @@
 import { expect, test } from "./testLib";
 import { average, sum } from "./stats";
+import * as statsAsync from "./statsAsync";
 
 test("Calculates the sum of all elements of the array", () => {
     const result = sum([1,2,3]);
@@ -9,6 +10,18 @@ test("Calculates the sum of all elements of the array", () => {
 
 test("Calculates the average of all elements of the array", () => {
     const result = average([1,2,3]);
+    const expected = 2;    
+    expect(expected).toBe(result)
+})
+
+test("Calculates the sum of all elements of the array (async)", async () => {
+    const result = await statsAsync.sum([1,2,3]);
+    const expected= 6;
+    expect(expected).toBe(result)
+})
+
+test("Calculates the average of all elements of the array (async)", async () => {
+    const result = await statsAsync.average([1,2,3]);
     const expected = 2;    
     expect(expected).toBe(result)
 })
