b350909783b6f3b221a0f72c903f71c04b6720d3
Darío Beiró <dario.beiro@dxc.com>
2023-04-04 17:29:16 +0200

    Refactor the assert method using callback

--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,15 +1,14 @@
-import { expect } from "./testLib";
+import { expect, test } from "./testLib";
 import { average, sum } from "./stats";
 
-let result: number, expected: number;
+test("Calculates the sum of all elements of the array", () => {
+    const result = sum([1,2,3]);
+    const expected= 6;
+    expect(expected).toBe(result)
+})
 
-result = sum([1,2,3]);
-expected = 6;
-
-expect(expected).toBe(result)
-
-result = average([1,2,3]);
-expected = 2;
-
-expect(expected).toBe(result)
-
+test("Calculates the average of all elements of the array", () => {
+    const result = average([1,2,3]);
+    const expected = 2;    
+    expect(expected).toBe(result)
+})
