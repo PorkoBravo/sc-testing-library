6bf7ad529a71c82830241a645950182ce7c0d0e2
Darío Beiró <dario.beiro@dxc.com>
2023-04-05 17:01:57 +0200

    Changing the expected and result value on assets because it was exchanged

--- src/tests/stats.test.ts
+++ src/tests/stats.test.ts
@@ -5,13 +5,13 @@ describe("Stats should", ()=> {
     it("Calculates the sum of all elements of the array", () => {
         const result = sum([1,2,3]);
         const expected= 6;
-        expect(expected).toBe(result)
+        expect(result).toBe(expected)
     })
     
     it("Calculates the average of all elements of the array", () => {
         const result = average([1,2,3]);
         const expected = 2;    
-        expect(expected).toBe(result)
+        expect(result).toBe(expected)
     })
 })
 
@@ -19,12 +19,12 @@ describe("Stats async should", ()=> {
     it("Calculates the sum of all elements of the array (async)", async () => {
         const result = await statsAsync.sum([1,2,3]);
         const expected= 6;
-        expect(expected).toBe(result)
+        expect(result).toBe(expected)
     })
     
     it("Calculates the average of all elements of the array (async)", async () => {
         const result = await statsAsync.average([1,2,3]);
         const expected = 2;    
-        expect(expected).toBe(result)
+        expect(result).toBe(expected)
     })
 })
